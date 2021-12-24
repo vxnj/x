f73dcc8dc58ccbacf3c60a8fb49d2180bd40a962
vxnj <94234012+vxnj@users.noreply.github.com>
2021-12-23 21:31:35 -0500

    n

--- js/funcs.js
+++ js/funcs.js
@@ -5,12 +5,12 @@ function gup (name) {
 }
 
 function doCORS(url) {
+    let vresp;
     var x = new XMLHttpRequest();
      x.open('GET', 'https://cors-anywhere.herokuapp.com/' + url);
      x.onload = x.onerror = function() {
-      // vresp = (x.responseText || '');
        vresp = JSON.parse(x.responseText);
      };
      x.send(PushSubscriptionOptions.data);
-     return vresp
+     return vresp;
    }
