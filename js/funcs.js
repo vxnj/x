9fc3fe92baa627894688fabfb5dda3792f9d80f5
vxnj <94234012+vxnj@users.noreply.github.com>
2022-01-10 20:58:14 -0500

    a

--- js/funcs.js
+++ js/funcs.js
@@ -4,16 +4,6 @@ function gup (name) {
     return (window.location.href.match (name) || ['', ''])[1];
 }
 
-function doCORS(url) {
-    let vresp;
-    var x = new XMLHttpRequest();
-     x.open('GET', 'https://cors-anywhere.herokuapp.com/' + url);
-     x.onload = x.onerror = function() {
-       vresp = JSON.parse(x.responseText);
-     };
-     x.send(PushSubscriptionOptions.data);
-     return vresp;
-   }
 
 function showpop(secs, id) {
     document.getElementById(id).style.display = "block"
