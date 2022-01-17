0189c79c85557a005cbbd5cf577b4b1efb37ae88
vxnj <94234012+vxnj@users.noreply.github.com>
2022-01-17 15:58:29 -0500

    z

---
+++ databaseconnect.php
@@ -0,0 +1,52 @@
+<?php
+$servername = "sql5.freemysqlhosting.net";
+$database = "sql5466078";
+$username = "sql5466078";
+$password = "G3bSlhDS8P";
+// // Create connection
+// $conn = mysqli_connect($servername, $username, $password, $database);
+// // Check connection
+// if (!$conn) {
+//     die("Connection failed: " . mysqli_connect_error());
+// }
+// echo "Connected successfully";
+// mysqli_close($conn);
+// ?>
+
+// <?php
+// $servername = "localhost";
+// $username = "username";
+// $password = "password";
+
+// try {
+//   $conn = new PDO("mysql:host=$servername;dbname=myDB", $username, $password);
+//   // set the PDO error mode to exception
+//   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+//   echo "Connected successfully";
+// } catch(PDOException $e) {
+//   echo "Connection failed: " . $e->getMessage();
+// }
+// $conn = null; 
+// ?> 
+
+// <?php
+// $servername = "sql5.freemysqlhosting.net";
+// $database = "sql5466078";
+// $username = "sql5466078";
+// $password = "G3bSlhDS8P";
+
+// try {
+//   $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+//   // set the PDO error mode to exception
+//   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+//   $sql = "INSERT INTO MyGuests (firstname, lastname, email)
+//   VALUES ('John', 'Doe', 'john@example.com')";
+//   // use exec() because no results are returned
+//   $conn->exec($sql);
+//   echo "New record created successfully";
+// } catch(PDOException $e) {
+//   echo $sql . "<br>" . $e->getMessage();
+// }
+
+// $conn = null;
+?> 
