5c36b03b138821fd018a7a6e915a1080e2b8c837
vxnj <94234012+vxnj@users.noreply.github.com>
2022-01-27 19:47:50 -0500

    a

--- db/listoAction.php
+++ db/listoAction.php
@@ -12,23 +12,29 @@ $id =   (empty($_GET['id']))   ? '' : $_GET['id'];
 $actn = (empty($_GET['actn'])) ? '' : $_GET['actn'];
 $sql = '';
 
+
 // Create connection & check
 require_once('../../resources/config.php');
 
 $conn = new mysqli( $servername, $username, $password, $database); 
 if ($conn->connect_error) {die("Connection failed: " . $conn->connect_error);}
+ 
+$id = str_replace('?id=', '', $id);
 
 switch ($actn) {
-    case 'add':  if($item>'') {$sql = "INSERT INTO listo (item, fin) VALUES ('" . $item . "', 0)";}  break;
-    case 'upd':  $sql = "UPDATE `listo` SET `item`='" . $item . "' WHERE id=?";   break;
-    case 'fin':  $sql = "UPDATE `listo` SET `fin`=now()            WHERE id=?";   break;
-    case 'und':  $sql = "UPDATE `listo` SET `fin`=0                WHERE id=?";   break;
-    case 'del':  $sql = "UPDATE `listo` SET `removed`='Y'          WHERE id=?";   break;
+    case 'add':  $sql = "INSERT INTO listo (item, fin) VALUES ( ? , 0 )";  break;
+    case 'upd':  $sql = "UPDATE `listo` SET `item`= ?         WHERE id= ? ";   break;
+    case 'fin':  $sql = "UPDATE `listo` SET `fin`=now()       WHERE id= ? ";   break;
+    case 'und':  $sql = "UPDATE `listo` SET `fin`=0           WHERE id= ? ";   break;
+    case 'del':  $sql = "UPDATE `listo` SET `removed`='Y'     WHERE id= ? ";   break;
     default:     $sql = ''; break;
 }
 
 $stmt = $conn->prepare($sql);
-$stmt->bind_param('i', $id);
+if      ($actn=='upd')  { $stmt->bind_param('si', $item, $id); }
+elseif  ($actn=='add')  { $stmt->bind_param('s',  $item); }
+else                    { $stmt->bind_param('i',  $id); }
+
 $stmt->execute();
 $result = $stmt->get_result();
 
