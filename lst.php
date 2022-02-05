924427ec4f6f59154f6f0b81a192abc3a53c100d
vxnj <94234012+vxnj@users.noreply.github.com>
2022-02-05 14:19:31 -0500

    a

--- lst.php
+++ lst.php
@@ -5,30 +5,35 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <link rel="icon" type="image/x-icon" href="img/lst.svg">  
-    <!-- <script src="js/funcs.js"></script> -->
-    <!-- <script src="js/fDate.js"></script> -->
     <link rel="stylesheet" href="/css/_main.css" />
     <link rel="stylesheet" href="/css/lst.css" />
     <title>Listo!</title>
 </head>
 
 <body>
-
 <header>
-    <input type="text" id="itemNew" placeholder="add item ..." autofocus>    
-</header>
+    <div class="myWidth">
+        <img id="currentUser" onclick="userSettings()"></img>
+        <input type="text" id="itemNew" placeholder="add item ..." autofocus>    
+    </div></header>
 
-<?php include ('db/lstRef.php'); ?>
-
+<?php 
+    include 'db/lstRef.php'; 
+    include "zzExamples/zzModal.html";
+    include "lstHeads.html" 
+?>
 <script>
+    document.title = 'LISTO 2!'
+    //console.log (window.innerWidth);
 
     //add
-    document.getElementById("itemNew").addEventListener('keypress', addnew)
+    document.getElementById("itemNew").addEventListener('keypress', addnew);
+    window.onload = getUser;
 
     //update
     let items = document.getElementsByClassName("itemopen");
     for (let i = 0; i < items.length; i++) {
-        items[i].addEventListener('focusout', doEdit );
+                items[i].addEventListener('focusout', doEdit );
                 items[i].addEventListener('keydown',  doEditK );
     }
 
@@ -37,14 +42,15 @@
         items[i].addEventListener('click', doBtn );
     }
     
-    
     let vx= 'as';
     function doBtn(e) { 
-        //console.log(e.currentTarget);
         id =   e.currentTarget.parentElement.id;
         actn = e.currentTarget.attributes.name.value.substring(3, 10);
-        
-        $.ajax({ 
+        if (actn == 'det'){ 
+            modal.style.display = "block";
+
+        } else {
+            $.ajax({ 
                 method: "POST",
                 url: "db/lstAct.php",
                 data: "actn=" + actn +"&id=" + id,
@@ -54,9 +60,9 @@
                     vx = xhr;
                     location.reload(); 
                 }
-       }); //ajax
-
-
+            }); //ajax
+        } //if
+       
     } //doBtn
 
     function doEditK(e) { if (e.code == 'Period' || e.code == 'Enter') {doEdit(e);} };
@@ -78,8 +84,7 @@
         $.ajax({ 
             type: "POST",
             url: "db/lstAct.php",
-            data: "actn=add&id=&item=" + e.target.value + "",
-
+            data: `actn=add&id=&item=${e.target.value}&usr=${usr}` ,
             success: function(output, status, xhr) {
                     console.log(xhr.status);
                     location.reload(); } ,
@@ -91,6 +96,39 @@
    
     }
 
+    
+
+
+    function userSettings() {
+        pick = document.getElementById('userSettings');
+        pick.style.display = (pick.style.display =='none') ? 'flex' : 'none';
+        if (pick.style.display == 'none') {pick.focus();}
+    }
+
+    let usr; let usrGet;
+    function getUser() {
+        usr =    localStorage.getItem("userx") || 'vx';
+        usrGet = `head-${usr}`;
+        document.getElementById("currentUser").src = `img/usrPics/${usrGet}.png`;
+        document.getElementById('userSettings').style.display = 'none'; 
+        newCol = `var(--${usrGet})`;
+        console.log (newCol);
+        document.documentElement.style.setProperty('--head0',newCol);
+        document.getElementById("itemNew").focus();
+      }
+
+    let f;
+    function setUser(e) {
+        f = e;
+        usr = f.target.id;
+        localStorage.setItem("userx", usr);
+        getUser();  
+    }       
+
+    let heads = document.getElementsByClassName("userhead");
+    for (let i = 0; i < heads.length; i++) {
+        heads[i].addEventListener('click', setUser)
+    }
 </script>
 
 </body>
