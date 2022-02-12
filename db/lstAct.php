3016835defd7406f67bef6063e41836567c01b11
vxnj <94234012+vxnj@users.noreply.github.com>
2022-02-12 18:31:54 -0500

    l

--- db/lstAct.php
+++ db/lstAct.php
@@ -21,11 +21,10 @@ switch ($actn) {
 }
 
 $stmt = $conn->prepare($sql);
-if      ($actn=='upd')  { $stmt->bind_param('si', $item, $id); }
+if      ($actn=='upd')  { $stmt->bind_param('s', $item, $id); }
 elseif  ($actn=='add')  { $stmt->bind_param('sss', $item, $usr, $cat); }
 elseif  ($actn!='qry')  { $stmt->bind_param('i',  $id); }
 $stmt->execute();
 $result = $stmt->get_result();
 
-    
 ?>
