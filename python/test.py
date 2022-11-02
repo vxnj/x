e97d332335150f5dbb81b677e0a3c6024e566b7b
vxnj <94234012+vxnj@users.noreply.github.com>
2022-11-02 10:23:54 -0400

    a

---
+++ python/test.py
@@ -0,0 +1,21 @@
+# fruits = ["apple", "banana", "cherry"]
+# for x in fruits:
+#   print(x)
+
+# x = 5
+# y = "John"
+# print(x)
+# print(y)
+
+# print(type(x))
+# print(type(y)) 
+
+import random
+
+
+
+print(random.randrange(1, 10))
+
+import numpy as np
+
+print(np.__version__)
