9cf3a11b1233be36f19639c5231109a8693f5fa1
vxnj <vxnj@x9.mailer.me>
2023-06-10 19:22:54 -0400

    a

--- mktdata.php
+++ mktdata.php
@@ -15,7 +15,7 @@ curl_setopt($c, CURLOPT_COOKIE, $yahoo_headers[$cookie_name]);
 curl_setopt($c, CURLOPT_RETURNTRANSFER, 1);
 $crumb = curl_exec($c);
 curl_close($c);
-echo "<BR>Crumb:" . $crumb;
+// echo "<BR>Crumb:" . $crumb;
 
 
 /* 3 - Get quotes with crumb, setting cookie. Using sample tickets*/
@@ -32,7 +32,7 @@ curl_close($c);
 /* 4 - Get data from yahoo */
 // $resJson_decode = json_decode($data_quote);
 
-echo $data_quote
+echo json_decode($data_quote)
 
 //  if (!$resJson_decode->quoteResponse->result) {
 //      $resultado = "Ticket dont exists in yahoo!";
